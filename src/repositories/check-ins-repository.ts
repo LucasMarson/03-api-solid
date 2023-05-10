7caf089c6fde2432e3d7c80109e2c36a4bb4010d
LucasMarson <lucas-marson@hotmail.com>
2023-05-09 22:04:56 -0300

    third commit

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,5 +3,6 @@ import { ChckIn, Prisma } from '@prisma/client'
 export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<ChckIn | null>
   findManyByUserId(userId: string, page: number): Promise<ChckIn[]>
+  countByUserId(userId: string): Promise<number>
   create(data: Prisma.ChckInUncheckedCreateInput): Promise<ChckIn>
 }
